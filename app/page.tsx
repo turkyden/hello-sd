e098e2ceb5ebb29ee61ef6f665aec812db936c60
Dengju Deng <wj871287@gmail.com>
2023-12-19 15:46:11 +0000

    fix: 新增请求调用

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,58 @@
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import fetch from 'node-fetch'
 
 export default function Home() {
+
+  const apiHost = process.env.API_HOST ?? 'https://api.stability.ai'
+  const url = `${apiHost}/v1/user/account`
+
+  const apiKey = process.env.STABILITY_API_KEY
+  if (!apiKey) throw new Error('Missing Stability API key.')
+
+  const [user, setUser] = useState({ email: '-'});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      })
+    
+      if (!response.ok) {
+        throw new Error(`Non-200 response: ${await response.text()}`)
+      }
+    
+      interface User {
+        id: string
+        profile_picture: string
+        email: string
+        organizations?: Array<{
+          id: string
+          name: string
+          role: string
+          is_default: boolean
+        }>
+      }
+    
+      // Do something with the user...
+      const user = (await response.json()) as User
+      setUser(user);
+    }
+
+    // call the function
+    fetchData()
+    // make sure to catch any error
+    .catch(console.error);
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+          Hello SD
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -29,14 +75,7 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
